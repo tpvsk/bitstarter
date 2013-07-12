1f796701a71571f17db0fad681a4c7baa251fcd5
Prasanna Tallapragada <tpvsk@yahoomail.com>
2013-07-12 07:12:10 +0000

    Updated_04 web.js

--- web.js
+++ web.js
@@ -4,10 +4,9 @@ var app = express.createServer(express.logger());
 
 var infile = "index.html";
 var buffer = new Buffer(27);
-//buffer.write(fs.readFileSync("index.html"));
-//buffer.write("Hello world from index.html", "utf-8");
+var fs = require("fs");
 
-buffer.write(fs.readFileSync('index.html'));
+buffer.write(fs.readFileSync("index.html", "utf-8"));
 
 app.get('/', function(request, response) {
   response.send(buffer.toString("utf-8"));
