c934e74d86468e7d51570655e24ed0a0a052add9
Prasanna Tallapragada <tpvsk@yahoomail.com>
2013-07-12 06:59:36 +0000

    Updated_01 web.js

--- web.js
+++ web.js
@@ -2,8 +2,13 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var infile = "index.html";
+var buffer = new Buffer(50);
+//buffer.write(fs.readFileSync("index.html"));
+buffer.write("Hello world from index.html", "utf-8");
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer.toString("utf-8"));
 });
 
 var port = process.env.PORT || 5000;
